7d62fc5cfbcd110aa1fc4782b1621b9f74b2fc18
knm-0125 <konami12525@gmail.com>
2025-12-29 21:53:13 +0900

    Delete kadai_010 directory

--- kadai_010/crud/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TopView, ProductDetailView
-
-urlpatterns = [
-    path('', TopView.as_view(), name='top'),
-    path('list/' ProductListView.as_view(), name='product_list')
-    path('detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-]
--- kadai_010/myproject/crud/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TopView, ProductDetailView
-
-urlpatterns = [
-    path('', TopView.as_view(), name='top'),
-    path('detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-]
