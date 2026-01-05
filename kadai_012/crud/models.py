7d9af0890e61aa5656e0aa0db7d87221911032a8
knm-0125 <konami12525@gmail.com>
2026-01-05 17:51:36 +0900

    Add files via upload

---
+++ kadai_012/crud/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    img = models.ImageField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('list')
